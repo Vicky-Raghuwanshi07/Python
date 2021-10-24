fba4623971690968c104fc966ff05afdb3b62d5e
Vicky-Raghuwanshi07 <74697810+Vicky-Raghuwanshi07@users.noreply.github.com>
2021-10-24 17:11:28 +0530

    Add files via upload
    A Hangman game using  Python , here user get 5 chances (i.e., 5 lives) to guess the correct letter and after every guess a hangman is been drawn which shows you the lives and different stages of Hangman . If all the lives are used or you are out of lives then lastly A Hanged Man shown which also shows you the lives at that condition you have 0 lives remaining.

---
+++ Hangman/hangman.py
@@ -0,0 +1,52 @@
+from words import word
+from hangman_draw import lives_draw
+import random
+import string
+
+def guess_words(word):
+	g_word = random.choice(word)
+	if "-" in g_word or " " in g_word:
+		g_word = random.choice(word)
+	
+	return g_word.upper()
+	
+def hangman():
+	s_word = guess_words(word)
+	set_word = set(s_word)
+	alphabet = set(string.ascii_uppercase)
+	used_word = set()
+	lives = 5
+	while len(set_word)>0 and lives>0:
+		print(lives_draw[lives])
+		print("\n\n")
+		word_letter = [l if l in used_word else "-" for l in s_word]
+		print("Word : "+ " ".join(word_letter))
+		print("You have remaining lives are : ",lives, "\nYour guessed letters are : ", " ".join(used_word))
+		user = input("Current Letter : ").upper()
+		
+		if user in alphabet - used_word:
+			used_word.add(user)
+			if user in set_word:
+				set_word.remove(user)
+			else:
+				lives-=1
+				print("Lives : ",lives)
+		elif user in used_word:
+			print("Already used this charater , plz type diffrent charater ")
+			print()
+		
+		else:
+			print("Invalid input")
+			print()
+		
+			
+	if lives==0:
+		print("\n\n")
+		print(lives_draw[lives])
+		print("\nSorry , You Loss\nThe right word is : ",s_word)
+	else:
+		print("\nYaah!! Correct guessed , The Word is :", s_word)
+		
+if __name__ == "__main__":
+	print("Welcome To Hangman Game".center(54,"-"))
+	hangman()
